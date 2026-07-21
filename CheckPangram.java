619e0467a84d3b7dc45b86d438bbba6c7adf9689
yaz342 <yazhinids0507@gmail.com>
2026-07-21 14:30:57 +0530

    Add files via upload

---
+++ CheckPangram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkIfPangram(String sentence){
+    
+        boolean[]letters=new boolean[26];
+
+
+       for(int i=0;i<sentence.length();i++){
+          char ch =sentence.charAt(i);
+          letters[ch  -  'a']=true;
+
+    }
+
+
+        for(int i=0;i<26;i++){
+          if(letters[i]==false){
+            return false;
+
+          }
+       }
+       return true;
+    }   
+}
